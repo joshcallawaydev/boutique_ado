4eb58f67c20a6304cf8ab02c915b65b74dd10b7b
joshcallawaydev <joshrcal89@gmail.com>
2022-03-06 20:39:36 +0000

    Added home page content

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
---
+++ home/views.py
@@ -0,0 +1,9 @@
+""" module """
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ A view to return the index page """
+    return render(request, 'home/index.html')
